2b8c2f691489126a46facb53a9e5a75e3d82293e
MemoOlv <guolg@ier.unam.mx>
2023-08-25 23:24:15 +0000

    🛑🧪 Fail tests

--- tests/initial_test.py
+++ tests/initial_test.py
@@ -1,7 +1,19 @@
-from model import nothing
+import panda as pd
+
+from model import nothing, multiplier
 
 
 def tests_nothing():
     obtained = nothing()
     expected = "nothing"
     assert obtained == expected
+
+
+def test_multiplier():
+    data = {"time": [1, 2, 3], "value": [3, 2, 1]}
+    df = pd.DataFrame(data)
+    times = 2
+    obtained = multiplier(df, times=times)
+    obtained_values = obtained.loc["values"]
+    expected_values = [6, 4, 2]
+    assert obtained_values == expected_values
